2ce48c8332c3ec9aada70b7d98ff665c00ba4422
Barry Dabrowski <dabrowski.barry@gmail.com>
2024-01-11 15:38:34 +0100

    Api Setup and SQLite DB is created

---
+++ Api/Program.cs
@@ -0,0 +1,37 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to connect the Database.
+builder.Services.AddDbContext<DataContext>(
+    Options => Options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConection"))
+);
+
+//Add Contoller
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(Options =>
+{
+    Options.AllowAnyOrigin();
+    Options.AllowAnyHeader();
+    Options.AllowAnyMethod();
+});
+
+app.UseHttpsRedirection();
+
+app.Run();
